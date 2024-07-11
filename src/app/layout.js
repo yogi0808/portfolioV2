b3dd35b6a6acf5930138b3d8103186a0885fe3fe
yogi0808 <gojiyayogesh08@gmail.com>
2024-07-11 16:46:53 +0530

    change matadata

--- src/app/layout.js
+++ src/app/layout.js
@@ -11,13 +11,17 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <meta property="og:image" content="/header.png"></meta>
-      <meta property="twitter:image" content="/header.png"></meta>
-      <meta property="og:site_name" content="Yogesh Portfolio Website"></meta>
+      <meta property="og:image" content="https://portfolio-v2-po2oj0lui-yogeshs-projects-05081680.vercel.app/header.png" />
+      <meta property="twitter:image" content="https://portfolio-v2-po2oj0lui-yogeshs-projects-05081680.vercel.app/header.png" />
+
+      <meta property="og:site_name" content="Yogesh Portfolio Website" />
+
       <meta property="og:description" content={metadata.description} />
-      <meta property="twitter:description" content={metadata.description}></meta>
-      <meta property="og:title" content={metadata.title}></meta>
-      <meta property="twitter:title" content={metadata.title}></meta>
+      <meta property="twitter:description" content={metadata.description} />
+
+      <meta property="og:title" content={metadata.title} />
+      <meta property="twitter:title" content={metadata.title} />
+
       <body className={inter.className}>{children}</body>
     </html>
   );
