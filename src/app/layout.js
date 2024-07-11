0cb8f97106ab6c57437cff432bf64c5425b49d11
yogi0808 <gojiyayogesh08@gmail.com>
2024-07-11 16:42:22 +0530

    add matadata

--- src/app/layout.js
+++ src/app/layout.js
@@ -11,6 +11,13 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <meta property="og:image" content="/header.png"></meta>
+      <meta property="twitter:image" content="/header.png"></meta>
+      <meta property="og:site_name" content="Yogesh Portfolio Website"></meta>
+      <meta property="og:description" content={metadata.description} />
+      <meta property="twitter:description" content={metadata.description}></meta>
+      <meta property="og:title" content={metadata.title}></meta>
+      <meta property="twitter:title" content={metadata.title}></meta>
       <body className={inter.className}>{children}</body>
     </html>
   );
