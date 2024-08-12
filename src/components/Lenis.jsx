049345959ddd67d2abcbb6b77cf9326b11204f95
yogi0808 <gojiyayogesh08@gmail.com>
2024-08-12 16:26:11 +0530

    modify lenis scroll

--- src/components/Lenis.jsx
+++ src/components/Lenis.jsx
@@ -1,10 +1,20 @@
 "use client"
 import React from "react"
-import { ReactLenis } from "lenis/react"
+import "lenis/dist/lenis.css"
+import { ReactLenis } from "lenis/dist/lenis-react"
 
 const Lenis = ({ children }) => {
   // Using lenis for Smooth Scroll
-  return <ReactLenis root>{children}</ReactLenis>
+  return (
+    <ReactLenis
+      root
+      options={{
+        lerp: 0.07,
+      }}
+    >
+      {children}
+    </ReactLenis>
+  )
 }
 
 export default Lenis
